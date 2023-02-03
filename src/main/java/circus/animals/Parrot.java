a810ebb857c474be1f313f3d4c0744a444f2e4db
lil1n <lilinongsc@gmail.com>
2023-02-03 17:42:02 +0800

    update code according to lecture instructins

---
+++ src/main/java/circus/animals/Parrot.java
@@ -0,0 +1,18 @@
+package circus.animals;
+
+public class Parrot extends Bird {
+    @Override
+    public String speak() {
+        return "Polly wants a cracker";
+    }
+
+    @Override
+    public String toString() {
+        return "I'm a parrot";
+    }
+
+    @Override
+    public int getValue() {
+        return 20;
+    }
+}
